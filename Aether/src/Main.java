4435e5082902664d853af33023e7ace8b440f2fd
Igor Queiroz <60364183+iqueiroz97@users.noreply.github.com>
2024-10-13 18:13:43 -0300

    Menu V1

--- Aether/src/Main.java
+++ Aether/src/Main.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner entradaJogador = new Scanner(System.in);
         Utils utils = new Utils();
+        Scanner entradaJogador = new Scanner(System.in);
 
         int opcaoSelecionada;
 
         utils.banner();
-        utils.terrorNoEspacoMorse();
-        System.out.println();
-
-
 
         do {
+
             utils.mostraMenu();
 
+            System.out.print("\nSelecione uma opção: ");
             opcaoSelecionada = entradaJogador.nextInt();
+
+            utils.opcoesMenu(opcaoSelecionada);
         } while (opcaoSelecionada != 4);
+
+        entradaJogador.close();
     }
 }
